949d6cdf9f92ea53a2adbead821f51e8d6ab8767
anikethsaha <anik220798@gmail.com>
2019-04-06 19:13:20 +0530

    Update store.js

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -1,13 +1,8 @@
 import { createStore }  from 'redux';
 import { reducer } from './reducer'
 
-const store = createStore(reducer);
-
-
 store.subscribe(async () =>
   console.log("> ACTION DISPATCHED <" , await store.getState())
 )
 
-module.exports = {
-    store
-};
+export const store = createStore(reducer);
